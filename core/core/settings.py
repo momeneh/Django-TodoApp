9667df7c373b42c2c3b5bd96a839feeab5d595d1
jafari <johndoe@example.com>
2026-08-17 10:55:42 +0330

    fix flake

--- core/core/settings.py
+++ core/core/settings.py
@@ -59,8 +59,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    #deployment needed render.com
-    "whitenoise.middleware.WhiteNoiseMiddleware", 
+    # deployment needed render.com
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
