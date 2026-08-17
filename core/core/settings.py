a0a344ffa0cfab95ec2124e88528f5f968b0ac45
jafari <johndoe@example.com>
2026-08-17 10:36:00 +0330

    add whitenoise for render.com

--- core/core/settings.py
+++ core/core/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", #deployment needed render.com
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -135,15 +136,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "/static/"
-MEDIA_URL = "/media/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-MEDIA_ROOT = BASE_DIR / "media"
 
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
 # rest framework setting
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
